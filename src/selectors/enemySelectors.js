77c4bd0fd02dfabaa20bc542e1848d8d58d56a91
Tobias Lundell <tobias.lundell@ericsson.com>
2020-02-01 18:53:14 +0100

    Remove unnecessary selectors

--- src/selectors/enemySelectors.js
+++ src/selectors/enemySelectors.js
@@ -6,12 +6,4 @@ export const getAllEnemyPositions = state =>
 export const getEnemy = (state, id) =>
   getEnemies(state).filter(enemy => enemy.id === id)[0];
 
-export const getEnemyPos = (state, id) => getEnemy(state, id)?.pos;
-
-export const getEnemyPosX = (state, id) => getEnemyPos(state, id)?.x || -1;
-
-export const getEnemyPosY = (state, id) => getEnemyPos(state, id)?.y || -1;
-
-export const getEnemyDirection = (state, id) => getEnemy(state, id)?.direction;
-
 export const getInterval = state => state.enemies.interval;
