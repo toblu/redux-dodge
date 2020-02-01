8bf14b19ed1ca2c2b2d71053ea948ced2f1cd7ce
Tobias Lundell <tobias.lundell@ericsson.com>
2020-02-01 17:51:43 +0100

    Updates

---
+++ precache-manifest.bfefe0bbaf959264ce371ed8bbd08c94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7918c7f05d196dcfcac1a423e20634c1",
+    "url": "/redux-dodge/index.html"
+  },
+  {
+    "revision": "085f2644e17f5e3eb3d0",
+    "url": "/redux-dodge/static/css/main.d3c6d57d.chunk.css"
+  },
+  {
+    "revision": "45f917adf5041d8a0b16",
+    "url": "/redux-dodge/static/js/2.b6b47973.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/redux-dodge/static/js/2.b6b47973.chunk.js.LICENSE"
+  },
+  {
+    "revision": "085f2644e17f5e3eb3d0",
+    "url": "/redux-dodge/static/js/main.bd606d41.chunk.js"
+  },
+  {
+    "revision": "976a22f57bc20095ed84",
+    "url": "/redux-dodge/static/js/runtime-main.4f40f62c.js"
+  },
+  {
+    "revision": "5a1a45169aa992161b81aca4c48b2d1e",
+    "url": "/redux-dodge/static/media/ARCADECLASSIC.5a1a4516.woff"
+  }
+]);
